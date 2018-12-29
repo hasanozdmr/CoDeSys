2ca16c6b0732767d2a6bfe19e71a1112b63b5569
olcayozdemir <olcay.ozdemir@evdekibakicim.com>
2018-12-29 14:33:42 +0300

    hdsjadhsajk

--- KEPCE/Program.cs
+++ KEPCE/Program.cs
@@ -8,9 +8,7 @@ namespace KEPCE
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
