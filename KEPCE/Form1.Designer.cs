45686ac7da145bbbb12f295f1942323dd324b179
hasanozdmr <hasanozdmr1903@gmail.com>
2018-12-29 14:20:03 +0300

    first commit

---
+++ KEPCE/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace KEPCE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbxGovde = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pbxKolD2 = new System.Windows.Forms.PictureBox();
+            this.pbxKolD3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGovde)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxKolD2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxKolD3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxGovde
+            // 
+            this.pbxGovde.BackColor = System.Drawing.Color.Transparent;
+            this.pbxGovde.Image = ((System.Drawing.Image)(resources.GetObject("pbxGovde.Image")));
+            this.pbxGovde.Location = new System.Drawing.Point(12, 98);
+            this.pbxGovde.Name = "pbxGovde";
+            this.pbxGovde.Size = new System.Drawing.Size(300, 208);
+            this.pbxGovde.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxGovde.TabIndex = 1;
+            this.pbxGovde.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(809, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            // 
+            // pbxKolD2
+            // 
+            this.pbxKolD2.BackColor = System.Drawing.Color.Transparent;
+            this.pbxKolD2.Image = ((System.Drawing.Image)(resources.GetObject("pbxKolD2.Image")));
+            this.pbxKolD2.Location = new System.Drawing.Point(306, 98);
+            this.pbxKolD2.Name = "pbxKolD2";
+            this.pbxKolD2.Size = new System.Drawing.Size(64, 197);
+            this.pbxKolD2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbxKolD2.TabIndex = 5;
+            this.pbxKolD2.TabStop = false;
+            // 
+            // pbxKolD3
+            // 
+            this.pbxKolD3.BackColor = System.Drawing.Color.Transparent;
+            this.pbxKolD3.Image = ((System.Drawing.Image)(resources.GetObject("pbxKolD3.Image")));
+            this.pbxKolD3.Location = new System.Drawing.Point(306, 98);
+            this.pbxKolD3.Name = "pbxKolD3";
+            this.pbxKolD3.Size = new System.Drawing.Size(138, 82);
+            this.pbxKolD3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbxKolD3.TabIndex = 6;
+            this.pbxKolD3.TabStop = false;
+            this.pbxKolD3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(929, 544);
+            this.Controls.Add(this.pbxKolD2);
+            this.Controls.Add(this.pbxGovde);
+            this.Controls.Add(this.pbxKolD3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGovde)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxKolD2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxKolD3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pbxGovde;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pbxKolD2;
+        private System.Windows.Forms.PictureBox pbxKolD3;
+    }
+}
+
---
+++ KEPCE/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEPCE
+{
+    public partial class Form1 : Form
+    {
+        int KonumX = 0, KonumY = 0, KonumX1 = 0, KonumY1 = 0, KonumX2 = 0, KonumY2 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            KonumX = pbxGovde.Location.X;
+            KonumY = pbxGovde.Location.Y;
+            KonumX1 = pbxKolD3.Location.X;
+            KonumY1 = pbxKolD3.Location.Y;
+            KonumX2 = pbxKolD2.Location.X;
+            KonumY2 = pbxKolD2.Location.Y;
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (KonumX < 0 && KonumX1 < 0 && KonumX2 < 0)
+            {
+                KonumX = 0;
+                KonumX1 = 0;
+                KonumX2 = 0;
+            }
+            else if (KonumX < 945 && KonumX1 < 945 && KonumX2 < 945)
+            {
+                KonumX = 945;
+                KonumX1 = 945;
+                KonumX2 = 945;
+            }
+            if (KonumY < 0 && KonumY1 < 0 && KonumY2 < 0)
+            {
+                KonumY = 0;
+                KonumY1 = 0;
+                KonumY2 = 0;
+            }
+            else if (KonumY > 583 && KonumY1 > 583 && KonumY2 > 583)
+            {
+                KonumY = 300;
+                KonumY1 = 300;
+                KonumY2 = 300;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                KonumX = KonumX - 5;
+                KonumX1 = KonumX1 - 5;
+                KonumX2 = KonumX2 - 5;
+                pbxGovde.Left = KonumX;
+                pbxKolD3.Left = KonumX1;
+                pbxKolD2.Left = KonumX2;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                KonumX = KonumX + 5;
+                KonumX1 = KonumX1 + 5;
+                KonumX2 = KonumX2 + 5;
+                pbxGovde.Left = KonumX;
+                pbxKolD3.Left = KonumX1;
+                pbxKolD2.Left = KonumX2;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                KonumY = KonumY - 5;
+                KonumY1 = KonumY1 - 5;
+                KonumY2 = KonumY2 - 5;
+                pbxGovde.Left = KonumY;
+                pbxKolD3.Left = KonumY1;
+                pbxKolD2.Left = KonumY2;
+
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                KonumY = KonumY + 5;
+                KonumY1 = KonumY1 + 5;
+                KonumY2 = KonumY2 + 5;
+                pbxGovde.Left = KonumY;
+                pbxKolD3.Left = KonumY1;
+                pbxKolD2.Left = KonumY2;
+            }
+        }
+    }
+}
